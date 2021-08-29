0652f0d88682240f90bfe5929da55dd7f603894b
Eric Nowac <enowac@gmail.com>
2021-08-29 13:23:22 -0500

    Added support for nicer commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,11 @@
 extern crate minifb;
-use gba_emulator::gba::GBA;
-use gba_emulator::gamepak::GamePack;
-use std::env;
-use std::io::prelude::*;
-use std::fs::File;
-use std::fs::OpenOptions;
-use log::{Record, Level, Metadata, SetLoggerError};
-use log::info;
-use std::time::Instant;
-use std::collections::VecDeque;
-use average::Mean;
-use std::cell::RefCell;
-use std::rc::Rc;
-
-
+use gba_emulator::{gamepak::GamePack, gba::GBA};
+use std::{fs::OpenOptions, io::prelude::*};
+use log::{Level, Metadata, Record, SetLoggerError, error, info};
+use std::{collections::VecDeque, time::Instant};
 use minifb::{Key, Window, WindowOptions};
+use clap::{AppSettings, Clap};
+use average::Mean;
 
 const WIDTH: usize = 240;
 const HEIGHT: usize = 160;
@@ -51,32 +42,47 @@ pub fn init_logger() -> Result<(), SetLoggerError> {
     Ok(())
 }
 
+#[derive(Clap)]
+#[clap(version = "1.0", author = "gba-rs team <https://github.com/gba-rs/>")]
+#[clap(setting = AppSettings::ColoredHelp)]
+struct Opts {
+    bios_file: String,
+    rom_file: String,
+    save_file: Option<String>,
+    #[clap(short, long)]
+    skip_bios: bool,
+    #[clap(short, long)]
+    frame_cap: Option<i32>,
+    #[clap(short, long)]
+    fps_counter: bool
+}
+
 fn main() {
+    let opts: Opts = Opts::parse();
+    let game_pack = GamePack::new(&opts.bios_file, &opts.rom_file);
 
-    match init_logger() {
-        Ok(_) => {
-            info!("Logger initialized succesfully");
-        },
-        Err(_) => {
-            info!("Logger failed to initialize");
+    let mut gba = if opts.skip_bios {
+        GBA::new(0x08000000, &game_pack)
+    } else {
+        GBA::new(0, &game_pack)
+    };
+
+    if let Some(ref save_path) = opts.save_file {
+        if let Ok(mut file) = OpenOptions::new().create(true).read(true).write(true).open(&save_path) {
+            let mut save_data: Vec<u8> = Vec::new();
+            let read_result = file.read_to_end(&mut save_data);
+            match read_result {
+                Ok(_) => {
+                    gba.load_save_file(&save_data);
+                    info!("Loaded save file {}", &save_path);
+                },
+                Err(_) => error!("Error reading {} to end", &save_path),
+            }
+        } else {
+            error!("Failed to open {}", &save_path);
         }
-    }
-
-    let args: Vec<String> = env::args().collect();
-    let mut game_pack = GamePack::new(&args[1], &args[2]);
-
-    // let mut gba: GBA = GBA::new(0x08000000, &game_pack);
-    let mut gba: GBA = GBA::new(0, &game_pack);
-
-    let mut save_file: RefCell<Option<File>> = RefCell::new(None);
-
-    if args.len() == 4 {
-        // game_pack.load_save_data(&args[3]);
-        save_file = RefCell::new(Some(OpenOptions::new().create(true).read(true).write(true).open(&args[3]).unwrap()));
-        let mut save_data: Vec<u8> = Vec::new();
-        save_file.borrow_mut().as_ref().unwrap().read_to_end(&mut save_data);
-        save_file.borrow_mut().as_ref().unwrap().seek(std::io::SeekFrom::Start(0));
-        gba.load_save_file(&save_data);
+    } else {
+        info!("No save file provided, will create");
     }
 
     let mut window = Window::new(
@@ -92,8 +98,11 @@ fn main() {
         panic!("{}", e);
     });
 
-    // Limit to max ~60 fps update rate
-    // window.limit_update_rate(Some(std::time::Duration::from_micros(16600)));
+    // Limit frame rate
+    if let Some(frame_cap) = opts.frame_cap {
+        let frame_time = (1.0 / (frame_cap as f32)) * 1_000_000.0;
+        window.limit_update_rate(Some(std::time::Duration::from_micros(frame_time as u64)));
+    }
 
     let mut fps_counter_buffer = VecDeque::new();
     let mut a: Mean = fps_counter_buffer.iter().collect();
@@ -126,16 +135,21 @@ fn main() {
             .update_with_buffer(&gba.gpu.frame_buffer, WIDTH, HEIGHT)
             .unwrap();
 
-        fps_counter_buffer.push_back(1f64 / now.elapsed().as_secs_f64());
-        if fps_counter_buffer.len() == FPS_BUFFER_SIZE {
-            a = fps_counter_buffer.drain(0..FPS_BUFFER_SIZE).collect();
+        if opts.fps_counter {
+            fps_counter_buffer.push_back(1f64 / now.elapsed().as_secs_f64());
+            if fps_counter_buffer.len() == FPS_BUFFER_SIZE {
+                a = fps_counter_buffer.drain(0..FPS_BUFFER_SIZE).collect();
+            }
+            window.set_title(&format!("GBA Emu: {} FPS", a.mean()));
         }
-
-        window.set_title(&format!("GBA Emu: {} FPS", a.mean()));
     }
     
 
-    if args.len() == 4 {
-        save_file.borrow_mut().as_ref().unwrap().write_all(&gba.get_save_data()[..]);
+    if let Some(ref save_path) = opts.save_file {
+        if let Ok(mut file) = OpenOptions::new().create(true).read(true).write(true).open(&save_path) {
+            let _ = file.write_all(&gba.get_save_data()[..]);
+        } else {
+            error!("Failed to open {}", &save_path);
+        }
     }
 }
